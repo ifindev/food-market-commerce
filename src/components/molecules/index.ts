23d5e53bf9d4bfe3be4e360a0dde31d79154a82a
Muhammad Arifin <muhammad.arifin@nri.co.id>
2023-05-11 23:43:39 +0700

    feat: create new implementation for Dropdown component

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,3 @@
+export { default as Dropdown } from './Dropdown/Dropdown.molecule'
 export { default as Header } from './Header/Header.molecule'
 export { default as InputText } from './InputText/InputText.Molecule'
